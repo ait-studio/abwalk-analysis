6ac08874a8f34f9d7266659255c664c7f8c576dc
hajun <audgkwns44@gmail.com>
2023-08-05 18:31:14 +0900

    dataprocessor - multi file handler

--- dataProcessor.py
+++ dataProcessor.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import os
 
 
 def main():
@@ -12,119 +13,143 @@ def main():
     bent_angle = []
     heights = []
 
-    with open(f"{csvFilePath}/{smapleFileName}", newline="") as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            right_arm_angle.append(row["right_arm_angle"])
-            left_arm_angle.append(row["left_arm_angle"])
-            left_waist_angle.append(row["left_waist_angle"])
-            right_waist_angle.append(row["right_waist_angle"])
-            step_length.append(row["step_length"])
-            step_speed.append(row["step_speed"])
-            bent_angle.append(row["bent_angle"])
-            heights.append(row["height"])
+    csvFilePath = "./analysed"
+    files = os.listdir(csvFilePath)
+    csvFiles = []
+    for file in files:
+        if file[-4:] == ".csv":
+            csvFiles.append(file)
 
-    sliceSize = 50
-    sliceCount = int(len(right_arm_angle) / sliceSize)
-    sliceCount = (
-        sliceCount
-        if sliceCount * 50 == len(right_arm_angle)
-        else sliceCount + 1
-        # If the length is the exactly multiplied number of {sliceCount}, we don't need to plus 1
-    )
+    resultData = []
 
-    # string to float
-    dataArray = [
-        right_arm_angle,
-        left_arm_angle,
-        left_waist_angle,
-        right_waist_angle,
-        step_length,
-        step_speed,
-        bent_angle,
-        heights,
-    ]
+    for file in csvFiles:
+        curResultData = []
+        with open(f"{csvFilePath}/{file}", newline="") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                right_arm_angle.append(row["right_arm_angle"])
+                left_arm_angle.append(row["left_arm_angle"])
+                left_waist_angle.append(row["left_waist_angle"])
+                right_waist_angle.append(row["right_waist_angle"])
+                step_length.append(row["step_length"])
+                step_speed.append(row["step_speed"])
+                bent_angle.append(row["bent_angle"])
+                heights.append(row["height"])
 
-    for data in dataArray:
-        for idx, row in enumerate(data):
-            data[idx] = float(row)
+        sliceSize = 50
+        sliceCount = int(len(right_arm_angle) / sliceSize)
+        sliceCount = (
+            sliceCount
+            if sliceCount * 50 == len(right_arm_angle)
+            else sliceCount + 1
+            # If the length is the exactly multiplied number of {sliceCount}, we don't need to plus 1
+        )
 
-    # arm angle average
+        # string to float
+        dataArray = [
+            right_arm_angle,
+            left_arm_angle,
+            left_waist_angle,
+            right_waist_angle,
+            step_length,
+            step_speed,
+            bent_angle,
+            heights,
+        ]
 
-    targetDataset = [right_arm_angle, left_arm_angle]
+        for data in dataArray:
+            for idx, row in enumerate(data):
+                data[idx] = float(row)
 
-    for idx, data in enumerate(targetDataset):
-        extractedArmAngles = []
-        for i in range(sliceCount):
-            startIndex = i * 50
-            endIndex = (i + 1) * 50 if (i + 1) * 50 < len(data) else len(data)
-            # print(startIndex, endIndex)
+        # arm angle average
 
-            curAngles = data[startIndex:endIndex]
-            curAngles = sorted(curAngles)
+        targetDataset = [right_arm_angle, left_arm_angle]
 
-            curAngleDiff = round((curAngles[-3] - curAngles[2]), 2)
+        for idx, data in enumerate(targetDataset):
+            extractedArmAngles = []
+            for i in range(sliceCount):
+                startIndex = i * 50
+                endIndex = (i + 1) * 50 if (i + 1) * 50 < len(data) else len(data)
+                # print(startIndex, endIndex)
 
-            extractedArmAngles.append(curAngleDiff)
+                curAngles = data[startIndex:endIndex]
+                curAngles = sorted(curAngles)
 
-        averageArmAngle = np.average(extractedArmAngles)
-        if idx == 0:
-            print("Right Arm Angle AVG", end="\t")
+                curAngleDiff = round((curAngles[-3] - curAngles[2]), 2)
+
+                extractedArmAngles.append(curAngleDiff)
+
+            averageArmAngle = np.average(extractedArmAngles)
+            if idx == 0:
+                print("Right Arm Angle AVG", end="\t")
+            else:
+                print("Left Arm Angle AVG", end="\t")
+            curAvgArmAngle = round(averageArmAngle, 2)
+            curResultData.append(curAvgArmAngle)
+            print(f"{curAvgArmAngle}°")
+
+        # Calculate Height-Pixel constant
+        meanHightPixels = np.average(heights[:10])  # pick only initial 10 values
+        realHeight = 170
+        heightFactor = 0.94  # nose node to toe doesn't mean the 100% of the height
+
+        heightPixelConstant = round((realHeight * heightFactor / meanHightPixels), 2)
+        print(f"Height Pixcel Constant\t{heightPixelConstant}cm/px")
+        curResultData.append(heightPixelConstant)
+
+        # step length
+        new_step_length = sorted(step_length)
+        extractedStepLength = round((new_step_length[-10] * heightPixelConstant), 2)
+        print(f"Extracted Step length \t{extractedStepLength}cm")
+        curResultData.append(extractedStepLength)
+
+        sortedAvgStepSpeed = sorted(step_speed)
+        selectedStepSpeed = []
+
+        if len(sortedAvgStepSpeed) >= 10:
+            selectedStepSpeed.append(sortedAvgStepSpeed[-10])
         else:
-            print("Left Arm Angle AVG", end="\t")
-        print(f"{round(averageArmAngle, 2)}°")
+            print("Can't get three highest value of step lengths data")
 
-    # Calculate Height-Pixel constant
-    meanHightPixels = np.average(heights[:10])  # pick only initial 10 values
-    realHeight = 170
-    heightFactor = 0.94  # nose node to toe doesn't mean the 100% of the height
+        avgSelectedStepSpeed = np.average(selectedStepSpeed)
+        unitConvertingConstant = 0.036  # c(m)ps to kph
+        stepSpeedFactor = 0.95
+        avgSelectedStepSpeed = round(
+            (
+                avgSelectedStepSpeed
+                * stepSpeedFactor
+                * unitConvertingConstant
+                * heightPixelConstant
+            ),
+            2,
+        )
 
-    heightPixelConstant = round((realHeight * heightFactor / meanHightPixels), 2)
-    print(f"Height Pixcel Constant\t{heightPixelConstant}cm/px")
+        print(f"Extracted Step Speed \t{avgSelectedStepSpeed}kph")
+        curResultData.append(avgSelectedStepSpeed)
 
-    # step length
-    new_step_length = sorted(step_length)
-    extractedStepLength = new_step_length[-10] * heightPixelConstant
-    print(f"Extracted Step length \t{round(extractedStepLength, 2)}cm")
+        # Step Asymmetry
+        avgLeftWaist = np.average(left_waist_angle)
+        avgRightWaist = np.average(right_waist_angle)
+        diffWaist = abs(avgLeftWaist - avgRightWaist)
+        avgWaist = (avgLeftWaist + avgRightWaist) / 2
+        asymmetryAmount = round((diffWaist / avgWaist) * 100, 2)
 
-    sortedAvgStepSpeed = sorted(step_speed)
-    selectedStepSpeed = []
+        print(f"Step Asymmetry is \t{asymmetryAmount}%")
+        curResultData.append(asymmetryAmount)
 
-    if len(sortedAvgStepSpeed) >= 10:
-        selectedStepSpeed.append(sortedAvgStepSpeed[-10])
-    else:
-        print("Can't get three highest value of step lengths data")
+        # Torso Bented
+        avgBented = round(np.average(bent_angle), 4)
+        print(f"Torso Bented amount is \t{avgBented}°")
+        curResultData.append(avgBented)
 
-    avgSelectedStepSpeed = np.average(selectedStepSpeed)
-    unitConvertingConstant = 0.036  # c(m)ps to kph
-    stepSpeedFactor = 0.95
-    avgSelectedStepSpeed = (
-        avgSelectedStepSpeed
-        * stepSpeedFactor
-        * unitConvertingConstant
-        * heightPixelConstant
-    )
+        resultData.append(curResultData)
 
-    print(f"Extracted Step Speed \t{round(avgSelectedStepSpeed, 2)}kph")
-
-    # Step Asymmetry
-    avgLeftWaist = np.average(left_waist_angle)
-    avgRightWaist = np.average(right_waist_angle)
-    diffWaist = abs(avgLeftWaist - avgRightWaist)
-    avgWaist = (avgLeftWaist + avgRightWaist) / 2
-    asymmetryAmount = round((diffWaist / avgWaist) * 100, 2)
-
-    print(f"Step Asymmetry is \t{asymmetryAmount}%")
-
-    # Torso Bented
-    avgBented = round(np.average(bent_angle), 4)
-    print(f"Torso Bented amount is \t{avgBented}Degree")
+    print(resultData)
 
     return True
 
 
-csvFilePath = "./analysed"
-smapleFileName = "03-90.avi.csv"
+# smapleFileName = "03-90.avi.csv"
 
 
 if __name__ == "__main__":
